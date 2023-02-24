362c6420f9a4a6fe46ff16c36d95071eef50fe9d
TecDroid 3354 <programacion@tecdroid3354.com>
2023-02-23 19:36:28 -0600

    Added Wrist and Poses to Arm

--- src/main/cpp/subsystems/Arm.cpp
+++ src/main/cpp/subsystems/Arm.cpp
@@ -2,24 +2,71 @@
 
 namespace TD {
 
-        Arm::Arm(uint8_t shoulderPort, uint8_t armPort):
-            shoulder(EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO>(shoulderPort)),
-            arm(EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO>(armPort)) {
+        Arm::Arm(uint8_t shoulderPort, uint8_t elbowPort, uint8_t wristPort):
+            m_shoulder(EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO>(shoulderPort)),
+            m_elbow(EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO>(elbowPort)),  
+            m_wrist(EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO>(wristPort)){
+                SetName("Arm");
+                m_shoulder.SetName("Shoulder");
+                m_elbow.SetName("Elbow");
+                m_wrist.SetName("Wrist");
 
         }
 
         void Arm::Periodic() {}
 
         frc2::CommandPtr Arm::SetShoulderAngle(units::angle::degree_t angle, double speed) {
-             return shoulder.SetAngleCmd(angle, speed);
+             return m_shoulder.SetAngleCmd(angle, speed);
         }
 
-        frc2::CommandPtr Arm::SetArmPosition(units::angle::degree_t angle, double speed) {
-            return arm.SetAngleCmd(angle, speed);
+        frc2::CommandPtr Arm::SetElbowAngle(units::angle::degree_t angle, double speed) {
+            return m_elbow.SetAngleCmd(angle, speed);
         }
 
-        frc2::CommandPtr Arm::SetSubsystemPosiotions(units::angle::degree_t shoulderAngle, units::angle::degree_t armAngle, double speed) {
-            return SetShoulderAngle(shoulderAngle, speed).AlongWith(SetArmPosition(armAngle, speed));
+        frc2::CommandPtr Arm::SetWristAngle(units::angle::degree_t angle, double speed) {
+            return m_wrist.SetAngleCmd(angle, speed);
+        }
+
+        frc2::CommandPtr Arm::SetPose(Poses pose) {
+            m_pose = Poses::kMoving;
+
+            // TEMP
+            return m_elbow.SetAngleCmd(0_deg,0);
+
+           //switch(pose){
+                // TODO       
+           //}
+        }
+
+        Arm::Poses Arm::GetPose(){
+            return m_pose;
+        }
+
+        std::string Arm::GetPoseStr(){
+            switch(m_pose){
+                case Poses::kMoving:
+                    return "Moving";
+                case Poses::kHome:
+                    return "Home";
+                case Poses::kTray:
+                    return "Tray";
+                case Poses::kLow:
+                    return "Low";
+                case Poses::kMiddle:
+                    return "Middle";
+                case Poses::kHigh:
+                    return "High";
+            }
+        }
+
+        void Arm::PrintPose(){
+            SmartDashboard::PutString(GetName()+" Pose",GetPoseStr());
+        }
+
+        void Arm::PrintAngles(){
+            m_shoulder.PrintPosition();
+            m_elbow.PrintPosition();
+            m_wrist.PrintPosition();
         }
 
 }
--- src/main/include/subsystems/Arm.h
+++ src/main/include/subsystems/Arm.h
@@ -6,20 +6,42 @@
 
 #include "subsystems/EncoderSubsystemBase.h"
 
+using namespace frc2;
+
 namespace TD {
     class Arm : public frc2::SubsystemBase {
     public:
-        Arm(uint8_t shoulderPort, uint8_t armPort);
+
+       
+        Arm(uint8_t shoulderPort, uint8_t elbowPort, uint8_t wristPort);
+
+        enum Poses{
+            kHome,
+            kLow,
+            kMiddle,
+            kHigh,
+            kTray,
+            kMoving,
+        };
 
         void Periodic() override;
 
-        frc2::CommandPtr SetShoulderAngle(units::angle::degree_t, double speed);
-        frc2::CommandPtr SetArmPosition(units::angle::degree_t, double speed);
-        frc2::CommandPtr SetSubsystemPosiotions(units::angle::degree_t, units::angle::degree_t, double speed);
+        CommandPtr SetShoulderAngle(units::angle::degree_t, double speed);
+        CommandPtr SetElbowAngle(units::angle::degree_t, double speed);
+        CommandPtr SetWristAngle(units::angle::degree_t, double speed);
+
+        CommandPtr SetPose(Poses);
+
+        void PrintPose();
+        void PrintAngles();
+        Poses GetPose();
+        std::string GetPoseStr();
 
     private:
-        EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO> shoulder;
-        EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO> arm;
+        EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO> m_shoulder;
+        EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO> m_elbow;
+        EncoderSubsystemBase<MotorTypes::SPARK, EncoderTypes::NEO> m_wrist;
 
+        Poses m_pose = Poses::kHome;
     };
 }
